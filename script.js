df6b5e32a4ed2df2713efd09b9d24d7c10aea83c
aditya379 <at37907@gmail.com>
2023-12-17 03:12:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+     /*   document.getElementById('searchButton').addEventListener('click', function(event) {
+            event.preventDefault(); 
+
+            const searchTerm = document.getElementById('searchInput').value;
+
+            if (searchTerm.trim() !== '') {
+                const googleSearchURL = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+                window.open(googleSearchURL,'_self');
+            }
+        });*/
+
+          
